9b4d41921d6db30bd9651b1d2f1c63bd82392f9b
hassanmonfared <hasandoulabi@gmail.com>
2024-03-04 03:56:00 +0330

    Add Readme.md file // Add sample data

---
+++ README.md
@@ -0,0 +1,11 @@
+### Task Experimental
+
+Hello! I'm glad to review your task. The project has been developed using the Go programming language, and efforts have been made to ensure that the project is modular. For testing, some data has been created in the database.
+
+### how deploy:
+
+``
+docker compose up -d
+``
+
+ If you need to make changes, you can refer to the ``config.yml`` for information such as database connection details.
--- repository/mysql/migrations/1709399510_add_seeder_data.sql
+++ repository/mysql/migrations/1709399510_add_seeder_data.sql
@@ -3,11 +3,14 @@ INSERT INTO `users` (`id`, `name`,`phone_number`) VALUES(1, 'hassan','0937756116
 
 INSERT INTO `vendors` (`id`, `name`) VALUES(1, 'chelopaz');
 INSERT INTO `vendors` (`id`, `name`) VALUES(2, 'kababi');
+INSERT INTO `vendors` (`id`, `name`) VALUES(3, 'pitza');
 
 
 INSERT INTO `agents` (`id`, `firstname`,`lastname`) VALUES(1, 'hossein','doulabi');
+INSERT INTO `agents` (`id`, `firstname`,`lastname`) VALUES(2, 'hassan','doulabi');
 
 INSERT INTO `orders` (`id`, `user_id`,`vendor_id`,`delivery_time`,`status`) VALUES(1, 1,1,50,'ready_to_send');
+INSERT INTO `orders` (`id`, `user_id`,`vendor_id`,`delivery_time`,`status`) VALUES(2, 1,2,50,'ready_to_send');
 
 
 -- +migrate Down
